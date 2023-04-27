8cff5611788bf784d4e206a3ed9bb82add762767
vibhanshu.jha3 <vibhanshu.jha3@gmail.com>
2023-04-28 02:32:25 +0530

    added controller method

--- src/main/java/com/themechguy/blogapp/controllers/postController.java
+++ src/main/java/com/themechguy/blogapp/controllers/postController.java
@@ -1,8 +1,11 @@
 package com.themechguy.blogapp.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.themechguy.blogapp.payloads.PostDto;
 import com.themechguy.blogapp.services.PostService;
+
+
 
 @RestController
 @RequestMapping("/api/")
@@ -26,5 +31,15 @@ public class postController {
 
         return new ResponseEntity<PostDto>(createdPost, HttpStatus.CREATED);
     }
-    
+    // get by user
+    @GetMapping("/user/{userId}/posts")
+    public ResponseEntity<List<PostDto>> getPostByUser(@PathVariable Long userId){
+        List<PostDto> posts =this.postService.getPostByUser(userId);
+        return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);   
+    }
+    //get posts by category
+    @GetMapping("/category/{catId}/posts")
+    public ResponseEntity<List<PostDto>> getPostByCategory(@PathVariable Long catId){
+        List<PostDto> posts =this.postService.getPostByCategory(catId);
+        return new ResponseEntity<List<PostDto>>(posts, HttpStatus.OK);}
 }
--- src/main/java/com/themechguy/blogapp/repositories/CategoryRepo.java
+++ src/main/java/com/themechguy/blogapp/repositories/CategoryRepo.java
@@ -1,9 +1,17 @@
 package com.themechguy.blogapp.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.themechguy.blogapp.entities.Category;
+import com.themechguy.blogapp.entities.Post;
+import com.themechguy.blogapp.entities.User;
 
 public interface CategoryRepo extends JpaRepository<Category, Long> {
+    
+    List<Post> findByUser(User user);
+    List<Post>findByCategory(Category category);
+    
     
 }
